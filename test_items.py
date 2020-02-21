38afa8b0b815db8cad940274f13d7b83cbb52187
Danil <danilkelembet@gmail.com>
2020-02-21 15:25:26 +0200

    Solved Excercise

--- test_items.py
+++ test_items.py
@@ -1,1 +1,9 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+
+def test_basket_btn_is_displayed(browser):
+    browser.get(link)
+    basket_btn = browser.find_element_by_css_selector("#add_to_basket_form > button")
+    assert basket_btn.is_displayed()
+    time.sleep(10)
